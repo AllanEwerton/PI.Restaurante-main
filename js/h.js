6ca4b4df370fcfca752747791e0fd5ab89e1ee7a
AllanEwerton <allanewerton>
2024-06-08 16:10:27 -0300

    teste de consumo de api fracasado :(

--- js/h.js
+++ js/h.js
@@ -1,26 +1,26 @@
 document.addEventListener("htmx:afterRequest", function(event){
 
-if(event.target.id === 'busca-produtos'){
+if(event.target.id === 'busca-pratos'){
     console.log(event.detail.xhr);
 
     const response = JSON.parse(event.detail.xhr.responseText)
-    const produtosDiv = document.querySelector("#dados-produtos");
+    const produtosDiv = document.querySelector("#dados-pratos");
 
     let htmlDiv = "";
     let style = "";
     let indice = 0;
     let preco = 0;
 
-    response.products.forEach(produto => {
+    response.pratos_principais.forEach(Menu => {
         style = indice%2==0 ? 'table-secondary' : ''
         
-        preco = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(produto.price);
+        preco = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(Menu.price);
         
         htmlDiv += `
         <tr class="${style}">
-         <td class="col-1"><img src="${produto.thumbnail}" width="100" class="img-thumbnail"></td>
-         <td class="col-1">${produto.id}</td>
-         <td class="col-8"><strong>${produto.title}</strong><br>${produto.description}</td>
+         <td class="col-1"><img src="${Menu.url}" width="100" class="img-thumbnail"></td>
+         <td class="col-1">${Menu.id}</td>
+         <td class="col-8"><strong>${Menu.nome}</strong><br>${Menu.descricao}</td>
          <td class="col-1">${preco}</td>
          <td class="col-1"><a href="#"><i style="color:red; font-size:20px" class="bi bi-trash-fill"></i></a></td>
         </tr>
